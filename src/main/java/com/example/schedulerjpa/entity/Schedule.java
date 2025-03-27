aae188f718de4244429896ed9e5cc2ace06fe2b3
sukh115 <sukh1151102@gmail.com>
2025-03-27 12:14:56 +0900

    feat : 엔티티 코드 구현

--- src/main/java/com/example/schedulerjpa/entity/Schedule.java
+++ src/main/java/com/example/schedulerjpa/entity/Schedule.java
@@ -1,4 +1,26 @@
 package com.example.schedulerjpa.entity;
 
-public class Schedule {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String contents;
 }
